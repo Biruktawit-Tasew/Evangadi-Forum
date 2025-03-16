3d6da24ff2160de1adadab9c37538aa035a4856b
Eneb <135270219+ametrahs@users.noreply.github.com>
2025-03-16 16:15:09 -0700

    signUp-component

--- client/src/Router.jsx
+++ client/src/Router.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 import Layout from "./components/Layout/Layout";
+import SignUp from "./components/SignUp/SignUp";
+import Home from "./pages/Home/Home";
 
 const Router = () => {
   return (
@@ -7,7 +9,9 @@ const Router = () => {
       <Routes>
         <Route path="/" element={<Layout />}>
           {/* Home page Route */}
-
+          <Route path="/" element={<Navigate to="/signup" />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/home" element={<Home />} />
           {/* Question page Route */}
 
           {/* Answer page Route */}
